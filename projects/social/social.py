3bd06363f3681d040ff1bcc83cf7dcc7bc64c605
Robert Carter <rc3331@gmail.com>
2020-03-18 17:15:00 -0500

    connections function now working

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,16 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -77,6 +90,28 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        q = Queue()
+
+        q.enqueue([user_id])
+
+        while q.size() > 0:
+            path = q.dequeue()
+
+            v = path[-1]
+
+            if v not in visited:
+                visited[v] = path
+
+            else:
+                if len(path) < len(visited[v]):
+                    visited[v] = path
+
+            for neighbor in self.friendships[v]:
+                if neighbor not in visited:
+                    path_copy = path.copy()
+                    path_copy.append(neighbor)
+                    q.enqueue(path_copy)
+
         return visited
 
 
@@ -85,4 +120,4 @@ if __name__ == '__main__':
     sg.populate_graph(10, 2)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
-    print(connections)
+    print("Connections:", connections)
